755f49c9bbb2355ec05d11fcd0ad74f7b18396e7
Phillip Verheyden <pverheyden@gmail.com>
2016-12-04 18:22:58 -0600

    BroadleafCommerce/QA#2125 - Add the a new annotation indicating whether or not templating exists

---
+++ src/main/java/org/broadleafcommerce/presentation/condition/ConditionalOnTemplating.java
@@ -0,0 +1,44 @@
+/*
+ * #%L
+ * BroadleafCommerce Common Presentation
+ * %%
+ * Copyright (C) 2009 - 2016 Broadleaf Commerce
+ * %%
+ * Licensed under the Broadleaf Fair Use License Agreement, Version 1.0
+ * (the "Fair Use License" located  at http://license.broadleafcommerce.org/fair_use_license-1.0.txt)
+ * unless the restrictions on use therein are violated and require payment to Broadleaf in which case
+ * the Broadleaf End User License Agreement (EULA), Version 1.1
+ * (the "Commercial License" located at http://license.broadleafcommerce.org/commercial_license-1.1.txt)
+ * shall apply.
+ * 
+ * Alternatively, the Commercial License may be replaced with a mutually agreed upon license (the "Custom License")
+ * between you and Broadleaf Commerce. You may not use this file except in compliance with the applicable license.
+ * #L%
+ */
+/**
+ * 
+ */
+package org.broadleafcommerce.presentation.condition;
+
+import org.springframework.context.annotation.Conditional;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Indicates that this bean should not be created unless templating configuration exists on the classpath. This can be used for beans
+ * that are designed to only exist on the presentation layer (like processors, dialects, variable expressions, template resolvers, etc)
+ * 
+ * @author Phillip Verheyden (phillipuniverse)
+ * @see {@link TemplatingExistsCondition}
+ */
+@Target({ ElementType.TYPE, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Conditional(TemplatingExistsCondition.class)
+public @interface ConditionalOnTemplating {
+
+}
