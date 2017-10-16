d0ec15748df5a478c74ac09f32ecbf53e5f1bf22
jefffischer <jfischer@broadleafcommerce.org>
2017-10-16 10:33:55 -0500

    Wrap Spring's BindStatus with our own version to better support both Spring 4 and Spring 5

---
+++ src/main/java/org/broadleafcommerce/presentation/model/BroadleafBindStatus.java
@@ -0,0 +1,42 @@
+/*
+ * #%L
+ * BroadleafCommerce Common Presentation
+ * %%
+ * Copyright (C) 2009 - 2017 Broadleaf Commerce
+ * %%
+ * Licensed under the Broadleaf Fair Use License Agreement, Version 1.0
+ * (the "Fair Use License" located  at http://license.broadleafcommerce.org/fair_use_license-1.0.txt)
+ * unless the restrictions on use therein are violated and require payment to Broadleaf in which case
+ * the Broadleaf End User License Agreement (EULA), Version 1.1
+ * (the "Commercial License" located at http://license.broadleafcommerce.org/commercial_license-1.1.txt)
+ * shall apply.
+ * 
+ * Alternatively, the Commercial License may be replaced with a mutually agreed upon license (the "Custom License")
+ * between you and Broadleaf Commerce. You may not use this file except in compliance with the applicable license.
+ * #L%
+ */
+package org.broadleafcommerce.presentation.model;
+
+import org.springframework.validation.Errors;
+
+/**
+ * @author Jeff Fischer
+ */
+public interface BroadleafBindStatus {
+
+    String getPath();
+    String getExpression();
+    Object getValue();
+    Class<?> getValueType();
+    Object getActualValue();
+    String getDisplayValue();
+
+    boolean isError();
+    String[] getErrorCodes();
+    String getErrorCode();
+    String[] getErrorMessages();
+    String getErrorMessage();
+    String getErrorMessagesAsString(String delimiter);
+    Errors getErrors();
+
+}
